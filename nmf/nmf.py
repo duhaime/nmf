fb57aa3b673e11c0efbbcfffafe4fad76576ea06
duhaime <douglas.duhaime@gmail.com>
2018-03-05 19:16:48 -0500

    upload to pypi

--- nmf/nmf.py
+++ nmf/nmf.py
@@ -7,7 +7,9 @@ import sys, glob, json, codecs, os, argparse
 import numpy as np
 
 class NMF:
-  def __init__(self, files=None, encoding='utf8', max_files=None, topics=20, iters=100, words=10, write_output=True):
+  def __init__(self, files=None, encoding='utf8', max_files=None,
+      topics=20, iters=100, words=10, write_output=False, min_count=2,
+      max_freq=0.95):
     self.files = files
     self.encoding = encoding
     self.max_files = max_files
@@ -15,15 +17,17 @@ class NMF:
     self.iters = iters
     self.words = words
     self.write_output = write_output
+    self.min_count = min_count
+    self.max_freq = max_freq
 
     # build corpus
     self.infiles = self.get_infiles(self.files, self.max_files)
     self.corpus = self.build_corpus(self.infiles, self.encoding)
-    
+
     # build tdm
     self.vectorizer = self.get_vectorizer(self.topics, self.words)
     self.tdm = self.get_tdm(self.vectorizer, self.corpus)
-    
+
     # factor tdm matrix
     self.feature_names = self.vectorizer.get_feature_names()
     self.nmf = self.build_nmf()
@@ -35,10 +39,10 @@ class NMF:
     # get topics by documents and topics by terms
     self.docs_to_topics = self.get_doc_to_topics()
     self.topics_to_words = self.get_topic_to_words()
-    
+
     # write the results
     if self.write_output:
-      self.write_results() 
+      self.write_results()
 
   def write_json(self, filename, obj):
     if not os.path.exists('results'):
@@ -76,9 +80,9 @@ class NMF:
     '''
     return TfidfVectorizer(
       input='content',
-      stop_words='english', 
-      max_df=0.95,
-      min_df=2,
+      stop_words='english',
+      max_df=self.max_freq,
+      min_df=self.min_count,
       max_features=topics * n_words * 1000
     )
 
@@ -95,7 +99,8 @@ class NMF:
   def get_documents_by_topics(self):
     np.seterr(divide='ignore', invalid='ignore')
     docs_by_topics = self.nmf.fit_transform(self.tdm)
-    return docs_by_topics / np.sum(docs_by_topics, axis=1, keepdims=True)
+    normalized = docs_by_topics / np.sum(docs_by_topics, axis=1, keepdims=True)
+    return np.nan_to_num(normalized) # zero out nan's
 
   def get_doc_to_topics(self):
     '''
@@ -114,7 +119,7 @@ class NMF:
     '''
     topic_to_words = defaultdict(list)
     for topic_id, topic in enumerate(self.topics_by_terms):
-      top_features = topic.argsort()[:-self.words - 1:-1] 
+      top_features = topic.argsort()[:-self.words - 1:-1]
       topic_to_words[topic_id] = [self.feature_names[i] for i in top_features]
     return topic_to_words
 
@@ -131,7 +136,10 @@ if __name__ == '__main__':
   parser.add_argument('-iters', default=100, type=int, help='number of iterations to run')
   parser.add_argument('-words', default=10, type=int, help='number of words to write per topic')
   parser.add_argument('-write_output', default=True, type=bool, help='specify whether to write output JSON')
+  parser.add_argument('-min_count', default=2, type=int, help='terms with >= min_count will be kept in the tdm')
+  parser.add_argument('-max_freq', default=0.95, type=float, help='terms occuring in > max_freq documents will be removed')
   args = parser.parse_args()
 
   NMF(files=args.files, encoding=args.encoding, max_files=args.max_files,
-      topics=args.topics, iters=args.iters, words=args.words, write_output=args.write_output)
+      topics=args.topics, iters=args.iters, words=args.words, write_output=args.write_output,
+      min_count=args.min_count, max_freq=args.max_freq)
