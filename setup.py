e6fbc13564516ac7f3fcaa9f9e9e534af420fd65
duhaime <douglas.duhaime@gmail.com>
2018-09-24 11:40:59 -0400

    use gte version checks for numpy + sklearn

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup (
   name='nmf',
-  version='0.0.5',
+  version='0.0.6',
   packages=['nmf'],
   keywords = ['data-mining', 'text-mining', 'topic-modeling', 'nmf'],
   description='Non-negative matrix factorization for building topic models in Python',
@@ -11,7 +11,7 @@ setup (
   author_email='douglas.duhaime@gmail.com',
   license='MIT',
   install_requires=[
-    'numpy==1.12.1',
-    'scikit-learn==0.18.1'
+    'numpy>=1.12.1',
+    'scikit-learn>=0.18.1'
   ],
 )
